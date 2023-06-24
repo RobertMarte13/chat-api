cd2f79d5bbf6db072a79e56edd7142243d32fbc8
RobertMarte13 <rproject133gmail.com>
2023-06-23 21:00:08 -0400

    Arreglando algunas cosas dela v0.1

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import path from 'path';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
 import indexRouter from './routes/index.routes.js'
+import { PORT } from './config.js';
 
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-app.use(cors())
 
 // middleware
 app.use(express.json())
@@ -23,5 +22,5 @@ app.use(indexRouter)
 //Routes public 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(3000)
+app.listen(PORT)
 console.log('Server listening on port 3000!')
