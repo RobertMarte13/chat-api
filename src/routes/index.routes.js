7b3fd2c1ed03b2eb0e40392fa9dc944080c744bb
RobertMarte13 <rproject133gmail.com>
2023-06-24 22:31:53 -0400

    v0.4 Mejorando el codigo de crear salas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -6,17 +6,30 @@ const router = Router();
 // SALAS
 router.get("/api/sala", async (req, res) => {
   const [rows] = await pool.query("SELECT * FROM sala");
+
   res.json(rows);
 });
 
 // CREATE SALA
 router.post("/api/sala", async (req, res) => {
-  const { sala } = req.body;
-  const [rows] = await pool.query("INSERT INTO sala(sala) VALUES (?)", [
-    sala,
-  ]);
+  const { sala, sala_id } = req.body;
+  
+  const data = await pool.query("SELECT * FROM sala");
 
-  res.json(rows);
+  const objets = data[0];
+
+  const validation = objets.find((res) => res.sala_id === sala_id);
+  console.log(validation);
+
+  if (validation === undefined) {
+    const [rows] = await pool.query(
+      "INSERT INTO sala(sala, sala_id) VALUES (?, ?)",
+      [sala, sala_id]
+    );
+    res.json(rows);
+  } else {
+    res.status(404).json({ message: "Try another name or id" });
+  }
 });
 
 // GET CHAT
